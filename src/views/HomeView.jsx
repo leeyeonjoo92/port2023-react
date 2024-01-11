c912609634d8e535c24fc41330d047228a0905ac
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-01-11 18:16:07 +0900

    스킬 영역 완료

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -7,6 +7,7 @@ import Port from '../component/Port'
 import Contact from '../component/Contact'
 import Footer from '../component/Footer'
 import Main from '../component/Main'
+import Site from '../component/Site'
 
 // 컴포넌트들은 하나의 div 태그 안에 들어와 있어야함
 // 쓸데없는 div가 싫으면 그냥 빈태그 <></>로 감싸도됨
@@ -18,6 +19,7 @@ const HomeView = () => {
 			<Main>
 				<Intro />
 				<Skill />
+				<Site />
 				<Port />
 				<Contact />
 			</Main>
