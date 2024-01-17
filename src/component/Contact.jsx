0423cfbd2d66f74a5dc4ae094a0058777de4a63a
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-01-17 17:38:29 +0900

    연락처 완료

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from "react";
+
+const contactText = [
+	{
+		link: "https://open.kakao.com/o/gM7YLzwf",
+		title: "KAKAO : webstupids",
+	},
+	{
+		link: "mailto:webstoryboy@naver.com",
+		title: "mail : webstoryboy@naver.com",
+	},
+];
 
 const Contact = () => {
 	return (
-		<div>Contact</div>
+		<section id="contact">
+			<div className="contact__inner">
+				<h2 className="contact__title">Contact</h2>
+				<div className="contact__lines top" aria-hidden="true">
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+				</div>
+				<div className="contact__text">
+					<div className="text">
+						{contactText.map((contact, key) => (
+							<div key={key}>
+								{/* noopener : 보안 속성 (새로 열린 페이지가 원래페이지에 접근 권한 못갖게) */}
+								{/* noreferrer : 개인정보 보호 (새로 열린 페이지가 어떤 사이트에서 링크클릭했는지 알수없게함) */}
+								<a href={contact.link} target="_blank" rel="noopener noreferrer">{contact.title}</a>
+							</div>
+						))}
+
+						{/* <div>
+							<a href="https://open.kakao.com/o/gM7YLzwf" target="_blank" rel="noopener noreferrer">KAKAO : Webstroyboy</a>
+						</div>
+						<div>
+							<a href="mailto:webstoryboy@naver.com" target="_blank" rel="noopener noreferrer">mail : WEBSTORYBOY@NAVER.COM</a>
+						</div> */}
+					</div>
+				</div>
+				<div className="contact__lines bottom" aria-hidden="true">
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+				</div>
+			</div>
+		</section>
 	)
 }
 
-export default Contact
+export default Contact;
