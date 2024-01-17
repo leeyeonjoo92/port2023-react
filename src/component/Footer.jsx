8f4a257aae34aa25cb540de57e780cbcb85dd3ed
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-01-17 18:08:43 +0900

    푸터완료

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,8 +1,109 @@
-import React from 'react';
+import React from "react";
+
+const footerText = [
+	{
+		title: "youtube",
+		desc: "유튜브에 오시면 더 많은 강의를 볼 수 있습니다.",
+		link: "https://www.youtube.com/@Webstoryboy",
+	},
+	{
+		title: "github",
+		desc: "깃헙에 오시면 더 많은 소스를 볼 수 있습니다.",
+		link: "https://github.com/webstoryboy",
+	},
+	{
+		title: "blog",
+		desc: "블러그에 오시면 더 많은 정보를 볼 수 있습니다.",
+		link: "https://webstoryboy.co.kr",
+	},
+	{
+		title: "gsap",
+		desc: "GSAP에 오시면 더 많은 강의를 볼 수 있습니다.",
+		link: "https://www.youtube.com/playlist?list=PL4UVBBIc6giL8-6jvrClimg0cFL-Muqiq",
+	},
+	{
+		title: "react",
+		desc: "리액트로 만든 사이트를 같이 만들어 봅니다.",
+		link: "https://github.com/webstoryboy/port2023-react",
+	},
+	{
+		title: "vue",
+		desc: "뷰로 만든 사이트를 같이 만들어 봅니다.",
+		link: "https://github.com/webstoryboy/port2023-vue",
+},
+	{
+		title: "next",
+		desc: "넥스트로 만든 사이트를 같이 만들어 봅니다.",
+		link: "https://github.com/webstoryboy/port2023-next",
+	},
+];
 
 const Footer = () => {
 	return (
-		<div>Footer</div>
+		<footer id="footer" role="contentinfo">
+			<div className="footer__inner">
+				<div className="footer__text">
+					<span>webstoryboy</span>
+					<span>© webs</span>
+				</div>
+				<div className="footer__info">
+					<div className="left">
+						<div className="title">
+							<a href="/">sign up</a>
+						</div>
+						<p>회원가입을 하시면 댓글과 게시판 기능을 이용할 수 있습니다.</p>
+					</div>
+					<div className="right">
+						<h3>social</h3>
+						<ul>
+							{footerText.map((footer, key) => (
+								<li key={key}>
+									<a href={footer.link}>{footer.title}</a>
+									<em>{footer.desc}</em>
+								</li>
+							))}
+
+							{/* <li>
+								<a href="https://www.youtube.com/@Webstoryboy">youtube</a>
+								<em>유튜브에 오시면 더 많은 강의를 볼 수 있습니다.</em>
+							</li>
+							<li>
+								<a href="https://github.com/webstoryboy">github</a>
+								<em>깃헙에 들어오시면 모든 소스를 볼 수 있습니다.</em>
+							</li>
+							<li>
+								<a href="https://webstoryboy.co.kr">tistory</a>
+								<em>티스토리에 들어오시면 좋은 정보 볼 수 있습니다.</em>
+							</li>
+							<li>
+								<a href="https://www.youtube.com/playlist?list=PL4UVBBIc6giL8-6jvrClimg0cFL-Muqiq">gsap</a>
+								<em>GSAP를 공부하시고 오면 도움이 됩니다.</em>
+							</li>
+							<li>
+								<a href="https://github.com/webstoryboy/port2023-vite">vite</a>
+								<em>비트 강의도 곧 오픈 예정입니다.</em>
+							</li>
+							<li>
+								<a href="https://github.com/webstoryboy/port2023-react">react</a>
+								<em>리액트 강의도 곧 오픈 예정입니다.</em>
+							</li>
+							<li>
+								<a href="https://github.com/webstoryboy/port2023-vue">vue</a>
+								<em>뷰 강의도 곧 오픈 예정입니다.</em>
+							</li>
+							<li>
+								<a href="https://github.com/webstoryboy/port2023-next">next</a>
+								<em>넥스트 강의도 곧 오픈 예정입니다.</em>
+							</li> */}
+						</ul>
+					</div>
+				</div>
+				<div className="footer__right">
+					© 2023 webstoryboy<br />
+					이 사이트는 리액트를 이용하여 제작하였습니다.
+				</div>
+			</div>
+		</footer>
 	)
 };
 
